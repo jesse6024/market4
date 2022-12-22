86a88c04b646b83bf16bf929c98f8fe82b5b7fbf
jesse6024 <60897213+jesse6024@users.noreply.github.com>
2022-12-22 15:19:40 -0500

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dashboard - Secured Page</title>
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="password-strength-indicator.css">
+</head>
+<body>
+<div class="container" style="">
+        <h1>REUP MARKET</h1>
+        <h5>Dashboard</h5>
+
+    </div>
+    <div class="container" style="">
+     
+
+     <header></header>
+     <form style="">
+<div class="form" style= " 
+    border:none;
+    width: 300px;
+    margin: 0 auto;
+    /*border: solid 1px black;*/
+    text-align: center;
+    margin-top: 15%;
+    overflow: hidden;
+">
+<h2>DASHBOARD</h2> 
+<div class="form" style="border:none;">
+<p>Dashboard</p>
+<p>This is another secured page.</p>
+<a href="logout.php">Logout</a>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Welcome</title>
+      <link rel="stylesheet" href="password-strength-indicator.css">
+      <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+<div class="container" style="
+    text-align:center;>
+
+    position: absolute;
+    background: #fff;
+    padding: 20px 30px;
+    width: 420px;
+    border-radius: 5px;
+    box-shadow: 0 0 15px rgb(0 0 0 / 20%);
+    top: 20px;
+                ">
+                <h1>REUP MARKET</h1>
+    </div>
+
+
+<h3>Welcome to the market</h3>
+<p>Please log in <a href="login.php">Login</a> or <a href="register.php">Register</a></p>
+</div>
+</body>
+</html>
